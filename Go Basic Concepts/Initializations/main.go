1e096720c775124d28f6d6e6230e71ae155f2ae4
natarajgootooru <nataraj.gootooru@gmail.com>
2021-01-07 20:20:35 +0530

    Add files via upload

---
+++ Go Basic Concepts/Initializations/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var (
+		country string
+		print   bool
+		name    string
+	)
+
+	var count, age int
+	_ = name
+
+	fmt.Println(count)
+	fmt.Println(age)
+	fmt.Printf("%q\n", country)
+	fmt.Println(print)
+}
